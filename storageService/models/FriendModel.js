0bf92d7837da2887dce35c1b548ea6903e8539dc
george_chen <bamoo456@gmail.com>
2015-03-12 21:00:39 +0800

    [Minor] add schema description

--- storageService/models/FriendModel.js
+++ storageService/models/FriendModel.js
@@ -2,8 +2,16 @@
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
 
+/**
+ * friendOwner, the uid of owner who has this friend
+ * uid, the friend uid
+ * nickName, the friend's nickName
+ * avatar, the avatar of this friend
+ * group, the group that this friend belong to
+ */
+
 var FriendSchmea = new Schema({
-    friendOwner:    {type : String, default : '', trim : true, index: true},  // specify the owner of this friend schema
+    friendOwner:    {type : String, default : '', trim : true, index: true},
     uid:            {type : String, default : '', trim : true},
     nickName:       {type : String, default : '', trim : true},
     avatar:         {type : String, default : '', trim : true},
