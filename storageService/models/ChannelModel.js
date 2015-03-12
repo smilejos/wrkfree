5614853e1c0f9d5d4e4d72dcc62471b763e28589
george_chen <bamoo456@gmail.com>
2015-03-12 20:55:36 +0800

    [Minor] add schema descripton

--- storageService/models/ChannelModel.js
+++ storageService/models/ChannelModel.js
@@ -2,32 +2,49 @@
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
 
+/**
+ * channelId, the channel's id
+ * host, the host of this channel
+ * name, the full name of this channel ex: "bamoo456@gmail.com#test"
+ * type, the type of channel,
+ *     NOTE: the "public" type of channel can be searched,
+ *           the "private" type of channel can not be searched
+ *           
+ * rtc.authorization, means channel members can start rtc or not
+ * drawing.boardNum, save how many draw board that are used
+ * msg.sender, the latest msg sender's uid
+ * msg.contents, the latest msg contents
+ * msg.timestamp, the latest msg's sent time
+ *
+ * 
+ * TODO: future support
+ * 
+ * openStatus, means that channel will be shown on home page or not
+ * organization, the name of organization that this channel belong to 
+ * anonymousEnter: means channel support anonymous login or not
+ * anonymousPassword: an password for anonymous login user
+ */
+
 var ChannelSchema = new Schema({
     channelId:          {type : String, default : '', trim : true, index: true},
     host:               {type : String, default : '', trim : true},
     name:               {type : String, default : '', trim : true},
-    type:               {type : String, default : '', trim : true},  // "public", "private"
-    rtc:                {   // now save rtc authorization status
+    type:               {type : String, default : '', trim : true},
+    rtc:                {
         authorization:  {type : Boolean, default : true, trim : true}
     },
-    drawing:            {   // now save current draw board number
+    drawing:            {
         boardNum:       {type : Number, default : 1, trim : true}
     },
-    msg:                {   // now save the latest msg 
+    msg:                {
         sender:         {type : String, default : '', trim : true},
         contents:       {type : String, default : '', trim : true},
-        timestamp:      {type : Date,   default : Date.now }
+        timestamp:      {type : Date,   default : new Date(0) }
     },
-
-    /**
-     * future support:
-     */
-    openStatus:         {
-        status:         {type : String, default : '', trim : true},   // "open", "organization"
-        info:           {type : String, default : '', trim : true},   // "orgization name"
-    },
-    anonymousEnter:     {type : Boolean, default : false, trim : true}, // set to "true" for anonymous login user
-    anonymousPassword:  {type : String, default : '', trim : true} // passowrd for anonymous login
+    openStatus:         {type : Boolean,default : false, trim : true},
+    organization:       {type : String, default : '', trim : true},
+    anonymousEnter:     {type : Boolean, default : false, trim : true},
+    anonymousPassword:  {type : String, default : '', trim : true}
 });
 
 ChannelSchema.set('autoIndex', false);
