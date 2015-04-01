8566dc9f10017aab6c3dabb57f5ce5896ee86828
george_chen <bamoo456@gmail.com>
2015-04-01 08:58:00 +0800

    [Impl] use a lightweight server to monitor web server

--- webServer/server/server.js
+++ webServer/server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 3000;
+var Env = process.env.NODE_ENV || 'development';
 
 // needed when we get the ".jsx" files
 require('node-jsx').install({
@@ -21,4 +22,11 @@ require('./configs/express')(server);
 require('./configs/routes')(server, StorageManager);
 
 server.listen(port);
-console.log('Listening on port ' + port);
+
+/**
+ * send an signal to gulp dev server to trigger livereload
+ */
+if (Env === 'development') {
+    var devPort = 9999;
+    require('net').connect(devPort);
+}
