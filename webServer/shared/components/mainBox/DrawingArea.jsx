5369456c190367a01d09b79cb41d5cbdeb99ae87
smilejos <smilejos.tung@gmail.com>
2015-04-28 00:07:36 +0800

    [Minor] adjust space

--- webServer/shared/components/mainBox/DrawingArea.jsx
+++ webServer/shared/components/mainBox/DrawingArea.jsx
@@ -11,7 +11,7 @@ module.exports = React.createClass({
     render: function(){
         return (
             <div className="mainBox" >
-            	<DrawingPalette />
+                <DrawingPalette />
                 <DrawingBoard />
             </div>
         );
