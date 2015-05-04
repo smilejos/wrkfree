abf4e40c022b5d3d170bf43ee3e8b6bcd9086986
george_chen <bamoo456@gmail.com>
2015-05-05 01:02:57 +0800

    [Impl] add onSaveDrawRecord action

---
+++ webServer/client/actions/draw/onSaveDrawRecord.js
@@ -0,0 +1,41 @@
+'use strict';
+var Promise = require('bluebird');
+var SharedUtils = require('../../../../sharedUtils/utils');
+var DrawTempStore = require('../../../shared/stores/DrawTempStore');
+
+/**
+ * @Public API
+ * @Author: George_Chen
+ * @Description: save the draw record when remote client published saveDrawRecord
+ *         NOTE: current client should check the number of draw chunks is 
+ *               the same as remote publisher.
+ * 
+ * @param {Object}      actionContext, the fluxible's action context
+ * @param {String}      data.channelId, target channel id
+ * @param {Number}      data.boardId, target board id
+ * @param {Number}      data.chunksNum, number of chunks in current record
+ * @param {Function}    callback, callback function
+ */
+module.exports = function(actionContext, data, callback) {
+    return Promise.props({
+        channelId: SharedUtils.argsCheckAsync(data.channelId, 'md5'),
+        boardId: SharedUtils.argsCheckAsync(data.boardId, 'boardId'),
+        chunksNum: SharedUtils.argsCheckAsync(data.chunksNum, 'number'),
+    }).then(function(validData) {
+        var drawTempStore = actionContext.getStore(DrawTempStore);
+        var tempRecord = drawTempStore.getDraws(data.channelId, data.boardId);
+        // TODO: re-pull lastest record from server ?
+        if (tempRecord.length !== validData.chunksNum) {
+            throw new Error('record is broken');
+        }
+        return actionContext.dispatch('ON_RECORD_SAVE', {
+            channelId: data.channelId,
+            boardId: data.boardId,
+            record: tempRecord
+        });
+    }).catch(function(err) {
+        SharedUtils.printError('onSaveDrawRecord.js', 'core', err);
+        return null;
+        // show alert message ?
+    }).nodeify(callback);
+};
