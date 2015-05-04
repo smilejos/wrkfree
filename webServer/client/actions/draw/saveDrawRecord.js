b82b203d09ecca7910b64794579119a587d4d0c5
george_chen <bamoo456@gmail.com>
2015-05-05 01:01:38 +0800

    [Impl] add saveDrawRecord action

---
+++ webServer/client/actions/draw/saveDrawRecord.js
@@ -0,0 +1,45 @@
+'use strict';
+var Promise = require('bluebird');
+var SharedUtils = require('../../../../sharedUtils/utils');
+var DrawService = require('../../services/drawService');
+var DrawTempStore = require('../../../shared/stores/DrawTempStore');
+
+/**
+ * @Public API
+ * @Author: George_Chen
+ * @Description: generate the draw record from draw chunks stored on drawTempStore
+ *         NOTE: chunksNum is used to notify server that how many chunks
+ *               that store on client side.
+ *               server should reply "true" if chunksNum is the same on both
+ *               client and server side
+ * 
+ * @param {Object}      actionContext, the fluxible's action context
+ * @param {String}      data.channelId, target channel id
+ * @param {Number}      data.boardId, target board id
+ * @param {Number}      data.chunksNum, number of chunks in current record
+ * @param {Function}    callback, callback function
+ */
+module.exports = function(actionContext, data, callback) {
+    return Promise.props({
+        channelId: SharedUtils.argsCheckAsync(data.channelId, 'md5'),
+        boardId: SharedUtils.argsCheckAsync(data.boardId, 'boardId'),
+        chunksNum: SharedUtils.argsCheckAsync(data.chunksNum, 'number'),
+    }).then(function(validData) {
+        return DrawService.saveRecordAsync(validData);
+    }).then(function(result) {
+        // TODO: failure handling
+        if (!result) {
+            throw new Error('save draw record fail');
+        }
+        var drawTempStore = actionContext.getStore(DrawTempStore);
+        return actionContext.dispatch('ON_RECORD_SAVE', {
+            channelId: data.channelId,
+            boardId: data.boardId,
+            record: drawTempStore.getDraws(data.channelId, data.boardId)
+        });
+    }).catch(function(err) {
+        SharedUtils.printError('saveDrawRecord.js', 'core', err);
+        return null;
+        // show alert message ?
+    }).nodeify(callback);
+};
