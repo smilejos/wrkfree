6d71839755c883d1d45f49e91e119e5a7991327f
george_chen <bamoo456@gmail.com>
2015-05-05 00:55:11 +0800

    [Impl] onDrawing is used to handle drawing request published from remote client

---
+++ webServer/client/actions/draw/onDrawing.js
@@ -0,0 +1,38 @@
+'use strict';
+var Promise = require('bluebird');
+var DrawUtils = require('./drawUtils');
+var SharedUtils = require('../../../../sharedUtils/utils');
+var WorkSpaceStore = require('../../../shared/stores/WorkSpaceStore');
+
+/**
+ * @Public API
+ * @Author: George_Chen
+ * @Description: onDrawing action, published by remote client
+ * 
+ * @param {Object}      actionContext, the fluxible's action context
+ * @param {String}      data.channelId, target channel id
+ * @param {Number}      data.boardId, target board id
+ * @param {Array}       data.chunks, the rawData of draw record
+ * @param {Function}    callback, callback function
+ */
+module.exports = function(actionContext, data, callback) {
+    return Promise.props({
+        channelId: SharedUtils.argsCheckAsync(data.channelId, 'md5'),
+        boardId: SharedUtils.argsCheckAsync(data.boardId, 'number'),
+        chunks: DrawUtils.checkDrawChunksAsync(data.chunks)
+    }).then(function(recordData) {
+        var workspaceStore = actionContext.getStore(WorkSpaceStore);
+        var state = workspaceStore.getState();
+        var cid = state.channel.channelId;
+        var bid = state.draw.currentBoardId;
+        if (cid === data.channelId && bid === data.boardId) {
+            return actionContext.dispatch('ON_DRAW_CHANGE', recordData);
+        }
+        // client and remote drawer use different board, so just send receive event
+        return actionContext.dispatch('ON_DRAW_RECEIVE', recordData);
+    }).catch(function(err) {
+        SharedUtils.printError('drawing.js', 'core', err);
+        return null;
+        // show alert message ?
+    }).nodeify(callback);
+};
