c75315004cadec8b5206d523f2ef732a33965073
george_chen <bamoo456@gmail.com>
2015-05-28 00:45:14 +0800

    [Minor] add environment while getting universal configs

--- socketServer/worker/worker.js
+++ socketServer/worker/worker.js
@@ -1,13 +1,14 @@
 'use strict';
 var Cookie = require('cookie');
 var Promise = require('bluebird');
+var Env = process.env.NODE_ENV || 'development';
 
 /**
  * setup configurations for each worker
  */
 var Configs = require('../../configs/config');
 Configs.import('params', require('../../configs/parameters.json'));
-Configs.import('db', require('../../configs/db.json'));
+Configs.import('db', require('../../configs/db.json')[Env]);
 
 var StorageManager = require('../../storageService/storageManager');
 // intialize db resource before internal modules
