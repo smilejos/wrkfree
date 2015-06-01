fb673edbad0bc87eadbf8cafafe7ba4130205d39
george_chen <bamoo456@gmail.com>
2015-06-01 18:07:30 +0800

    [Impl] refactor the flow of attaching rtc media stream

--- webServer/shared/components/common/rtcVideo.jsx
+++ webServer/shared/components/common/rtcVideo.jsx
@@ -1,41 +1,29 @@
 var React = require('react');
 var AttachStream = require('attachmediastream');
-var CryptoUtils = require('../../../../sharedUtils/cryptoUtils');
 
 /**
  * the basic rtc video container
  */
 module.exports = React.createClass({
-    getInitialState: function() {
-        return {
-            videoId: null
-        };
-    },
-
     /**
-     * generate a timebased video element id
+     * attach the media stream to video element
      */
     componentDidMount: function(){
-        var timeStr = Date.now().toString();
-        this.setState({
-            videoId: CryptoUtils.getMd5Hex(timeStr)
-        });
+        if (this.props.stream) {
+            AttachStream(
+                this.props.stream, 
+                document.getElementById(this.props.videoId)
+            );
+        }
     },
 
     render: function(){
-        var stream = this.props.stream;
-        var videoId = this.state.videoId;
-        if (stream) {
-            AttachStream(stream, document.getElementById(videoId));
-        }
         return (
             <div className="rtcVideo">
                 <video 
                     width="250" 
-                    id={videoId}
-                    muted
-                    controls
-                    className={this.props.isVisible ? 'video-shown' : 'video-hidden'} />
+                    id={this.props.videoId}
+                    muted />
             </div>
         );
     }
