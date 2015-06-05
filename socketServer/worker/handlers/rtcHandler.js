c992a9d7a7767d075c5202e606a7ed550e8f6b12
george_chen <bamoo456@gmail.com>
2015-06-05 10:52:17 +0800

    [Impl] add rtc handler on server side

---
+++ socketServer/worker/handlers/rtcHandler.js
@@ -0,0 +1,102 @@
+'use strict';
+var Promise = require('bluebird');
+var SharedUtils = require('../../../sharedUtils/utils');
+var StorageManager = require('../../../storageService/storageManager');
+var RtcStorage = StorageManager.getService('Rtc');
+var RtcWorker = require('../services/rtcWorker');
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: for user to start his conference staet on current channel
+ *       
+ * @param {Object}          socket, the client socket instance
+ * @param {String}          data.channelId, the channel id
+ */
+exports.startConferenceAsync = function(socket, data) {
+    return Promise.join(
+        RtcStorage.getSessionAsync(data.channelId),
+        SharedUtils.argsCheckAsync(data.channelId, 'md5'),
+        function(session, cid) {
+            var sdp = _getDefaultSdp();
+            return RtcStorage.addClientAsync(cid, socket.id, sdp)
+                .then(function() {
+                    return (!session ? RtcWorker.pushNotification(cid) : true);
+                });
+        }).catch(function(err) {
+        SharedUtils.printError('rtcHandler.js', 'startConferenceAsync', err);
+        throw err;
+    });
+};
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: for user to hangup channel conference
+ *       
+ * @param {Object}          socket, the client socket instance
+ * @param {String}          data.channelId, the channel id
+ */
+exports.hangupConferenceAsync = function(socket, data) {
+    return SharedUtils.argsCheckAsync(data.channelId, 'md5')
+        .then(function(cid) {
+            return RtcStorage.delClientAsync(cid, socket.id);
+        }).then(function(success) {
+            if (!success) {
+                throw new Error('server storage get failure');
+            }
+            return success;
+        }).catch(function(err) {
+            SharedUtils.printError('rtcHandler.js', 'hangupConferenceAsync', err);
+            throw err;
+        });
+};
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: for user to get session description of target users
+ *         NOTE: caller need to get the callee's sdp for signaling
+ *       
+ * @param {Object}          socket, the client socket instance
+ * @param {String}          data.channelId, the channel id
+ * @param {Array}          data.targets, target client sockets
+ */
+exports.getTargetsSdpAsync = function(socket, data) {
+    return Promise.join(
+        RtcStorage.getSessionAsync(data.channelId),
+        SharedUtils.argsCheckAsync(data.targets, 'array'),
+        SharedUtils.argsCheckAsync(data.channelId, 'md5'),
+        function(session, targets) {
+            var sdps = {};
+            SharedUtils.fastArrayMap(targets, function(client) {
+                sdps[client] = session.sdps[client];
+            });
+            return sdps;
+        }).catch(function(err) {
+            SharedUtils.printError('rtcHandler.js', 'getTargetsSdp', err);
+            throw err;
+        });
+};
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: used for conference signaling
+ *         NOTE: currently do nothing on this API
+ */
+exports.signaling = function() {
+    // current do nothing
+};
+
+/**
+ * @Author: George_Chen
+ * @Description: get the default sdp of socket client
+ */
+function _getDefaultSdp() {
+    return {
+        video: true,
+        audio: false,
+        screen: false
+    };
+}
