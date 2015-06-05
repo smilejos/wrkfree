05fd0b9505d9de645d257358ca669ba5c5d32bfe
george_chen <bamoo456@gmail.com>
2015-06-05 11:08:52 +0800

    [Impl] add container view of remote conference clients

---
+++ webServer/shared/components/rightBox/ConferenceArea.jsx
@@ -0,0 +1,63 @@
+var React = require('react');
+var FluxibleMixin = require('fluxible').Mixin; 
+var SharedUtils = require('../../../../sharedUtils/utils');
+
+/**
+ * store
+ */
+var ConferenceStore = require('../../stores/ConferenceStore');
+
+/**
+ * child components
+ */
+var RtcVideo = require('../common/rtcVideo.jsx');
+
+/**
+ * @Author: George_Chen
+ * @Description: conference area component used to render remote rtc view
+ *
+ * @param {String}      this.props.channelId, channel's id
+ */
+module.exports = React.createClass({
+    mixins: [FluxibleMixin],
+    statics: {
+        storeListeners: {
+            '_onStoreChange': [ConferenceStore]
+        }
+    },
+
+    _onStoreChange: function(){
+        var state = this._getStoreState();
+        this.setState(state);
+    },
+
+    _getStoreState: function() {
+        var channelId = this.props.channelId;
+        return this.getStore(ConferenceStore).getState(channelId);
+    },
+
+    getInitialState: function() {
+        return this._getStoreState();
+    },
+
+    render: function() {
+        var conference = this.state.conference;
+        var rtcComponents = '';
+        if (conference) {
+            var participants = Object.keys(conference);
+            rtcComponents = SharedUtils.fastArrayMap(participants, function(clientId){
+                return (
+                    <RtcVideo 
+                        key={clientId}
+                        videoId={clientId} 
+                        stream={conference[clientId]} />
+                );
+            });
+        }
+        return (
+            <div className="ConferenceArea">
+                {rtcComponents}
+            </div>
+        );
+    }
+});
