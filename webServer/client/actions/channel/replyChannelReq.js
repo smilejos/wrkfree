2fa531870ed14bf04e9b45bc905eb66b2a681a59
george_chen <bamoo456@gmail.com>
2015-06-11 14:33:49 +0800

    [Impl] delete notification when client reply channel/friend it done

--- webServer/client/actions/channel/replyChannelReq.js
+++ webServer/client/actions/channel/replyChannelReq.js
@@ -9,24 +9,27 @@ var SharedUtils = require('../../../../sharedUtils/utils');
  * @Description: action for channel host to reply channel request
  * 
  * @param {Object}      actionContext, the fluxible's action context
- * @param {String}      data.reqId, the request id
- * @param {String}      data.targetUser, the uid of channel request sender
+ * @param {String}      data.id, the notification id
+ * @param {String}      data.target, the uid of friend request sender
  * @param {String}      data.channelId, the target channel id
- * @param {Boolean}     data.isPermitted, the answer of this response
+ * @param {Boolean}     data.permit, the answer of this response
  */
 module.exports = function(actionContext, data) {
     return Promise.props({
-        reqId: SharedUtils.argsCheckAsync(data.reqId, '_id'),
-        respTarget: SharedUtils.argsCheckAsync(data.targetUser, 'md5'),
+        reqId: SharedUtils.argsCheckAsync(data.id, '_id'),
+        respTarget: SharedUtils.argsCheckAsync(data.target, 'md5'),
         channelId: SharedUtils.argsCheckAsync(data.channelId, 'md5'),
-        isPermitted: SharedUtils.argsCheckAsync(data.isPermitted, 'boolean'),
+        isPermitted: SharedUtils.argsCheckAsync(data.permit, 'boolean')
     }).then(function(reqData) {
         return ReqRespService.channelRespAsync(reqData);
     }).then(function(result) {
         if (!result) {
             throw new Error('response channel request fail');
         }
-        // TODO: response channel request success
+        actionContext.dispatch('DELETE_NOTIFICATION', {
+            reqId: data.id,
+            isReq: true
+        });
     }).catch(function(err) {
         SharedUtils.printError('replyChannelReq.js', 'core', err);
     });
--- webServer/client/actions/friend/replyFriendReq.js
+++ webServer/client/actions/friend/replyFriendReq.js
@@ -9,22 +9,25 @@ var SharedUtils = require('../../../../sharedUtils/utils');
  * @Description: action for reply friend request
  * 
  * @param {Object}      actionContext, the fluxible's action context
- * @param {String}      data.reqId, the request id
- * @param {String}      data.targetUser, the uid of friend request sender
- * @param {Boolean}     data.isPermitted, the answer of this response
+ * @param {String}      data.id, the notification id
+ * @param {String}      data.target, the uid of friend request sender
+ * @param {Boolean}     data.permit, the answer of this response
  */
 module.exports = function(actionContext, data) {
     return Promise.props({
-        reqId: SharedUtils.argsCheckAsync(data.reqId, '_id'),
-        respTarget: SharedUtils.argsCheckAsync(data.targetUser, 'md5'),
-        isPermitted: SharedUtils.argsCheckAsync(data.isPermitted, 'boolean')
+        reqId: SharedUtils.argsCheckAsync(data.id, '_id'),
+        respTarget: SharedUtils.argsCheckAsync(data.target, 'md5'),
+        isPermitted: SharedUtils.argsCheckAsync(data.permit, 'boolean')
     }).then(function(reqData) {
         return ReqRespService.friendRespAsync(reqData);
     }).then(function(result) {
         if (!result) {
             throw new Error('response friend request fail');
         }
-        // TODO: response friend request success
+        actionContext.dispatch('DELETE_NOTIFICATION', {
+            reqId: data.id,
+            isReq: true
+        });
     }).catch(function(err) {
         SharedUtils.printError('replyFriendReq.js', 'core', err);
     });
