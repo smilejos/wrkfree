075b2d5eb805de5dfa0caa20f85450c1a58bc7d8
george_chen <bamoo456@gmail.com>
2015-06-11 15:11:45 +0800

    [Impl] add components for each type of notification.        e.g. normal, request, response

---
+++ webServer/shared/components/common/normalNotice.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var FluxibleMixin = require('fluxible').Mixin;
+
+/**
+ * child components
+ */
+var NoticeMessage = require('./noticeMessage.jsx');
+
+/**
+ * @Author: George_Chen
+ * @Description: the normal notice component
+ *         
+ * @param {Object}      this.props.info, the notice info
+ */
+module.exports = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <NoticeMessage message={''}
+                    title={'normal notice'} />
+            </div>
+        );
+    }
+});
