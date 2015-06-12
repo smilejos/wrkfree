9e6e69b024772c3d8c3305d53e72360eb56ad858
george_chen <bamoo456@gmail.com>
2015-06-12 22:31:35 +0800

    [Impl] update install script

--- npm-install.sh
+++ npm-install.sh
@@ -5,11 +5,14 @@
 ##########################################################
 cd ./sharedUtils/ && npm install -d
 cd ../
-echo '============   sharedUtils npm installed ==========='
+echo '============   sharedUtils npm installed =============='
 cd ./storageService/ && npm install -d
 cd ../
 echo '============   storageService npm installed ==========='
+cd ./socketServer/ && npm install -d
+cd ../
+echo '============   socket-server npm installed ============'
 cd ./webServer/ && npm install -d
-echo '============   web-server npm installed ============'
+echo '============   web-server npm installed ==============='
 npm start
-echo '============== client app build done ==============='
+echo '============== client app build done =================='
