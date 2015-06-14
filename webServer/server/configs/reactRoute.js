d5db523866f4fb4ef98cfa7b82a2f6755b99ef61
george_chen <bamoo456@gmail.com>
2015-06-14 08:27:28 +0800

    [Impl] modify render mechanism for supporting lastest fluxible.io

--- webServer/server/configs/reactRoute.js
+++ webServer/server/configs/reactRoute.js
@@ -5,6 +5,7 @@ var NavigateAction = require('../../shared/navigateAction');
 var Serialize = require('serialize-javascript');
 var App = require('../../shared/app');
 
+var FluxibleComponent = require('fluxible/addons/FluxibleComponent');
 var HtmlComponent = React.createFactory(require('../../shared/components/Html.jsx'));
 
 module.exports = function(req, res) {
@@ -13,23 +14,24 @@ module.exports = function(req, res) {
     context.getActionContext().setRouteInfo(req.routeInfo);
 
     // start the react-router
-    Router.run(App.getAppComponent(), req.path, function(Handler, state){
+    Router.run(App.getComponent(), req.path, function(Handler, state) {
 
-        context.executeAction(NavigateAction, state, function(){
+        context.executeAction(NavigateAction, state, function() {
             // for all registered stores, call the 'dehydrate' function
             var exposed = 'window.App=' + Serialize(App.dehydrate(context)) + ';';
-
-            React.withContext(context.getComponentContext(), function(){
-                // render the appComponent to an 'html' template
-                var html = React.renderToStaticMarkup(HtmlComponent({
-                    state: exposed,
-                    host: 'https://' + req.get('host') + '/app/build/',
-                    markup: React.renderToString(React.createFactory(Handler)())
-                }));
-                res.write(html);
-                res.end();
-            });
+            var component = React.createFactory(Handler);
+            var props = {
+                context: context.getComponentContext()
+            };
+            var html = React.renderToStaticMarkup(HtmlComponent({
+                state: exposed,
+                host: 'https://' + req.get('host') + '/app/build/',
+                markup: React.renderToString(
+                    React.createElement(FluxibleComponent, props, component())
+                )
+            }));
+            res.send(html);
+            res.end();
         });
     });
 };
-
