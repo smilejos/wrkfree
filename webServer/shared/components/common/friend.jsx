b32019fa2542d958fd7b2cff22cb0d9d820490e6
george_chen <bamoo456@gmail.com>
2015-07-03 14:47:52 +0800

    [Impl] refactor trackFriendActivity to track one friend at a time

--- webServer/shared/components/common/friend.jsx
+++ webServer/shared/components/common/friend.jsx
@@ -107,7 +107,7 @@ module.exports = React.createClass({
             friendUid: uid
         });
         this.executeAction(TrackFriendActivity, {
-            friendUids: [uid]
+            friendUid: uid
         });
         this.executeAction(SubscribeChannelNotification, {
             channels: [cid]
