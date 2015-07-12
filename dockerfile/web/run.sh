cbbb6fd0dec574fe27426d718c2180c7f96eef09
george_chen <bamoo456@gmail.com>
2015-07-12 15:06:33 +0800

    [Impl] remove default run.sh on both web and ws Dockerfile,        this is for node to catching stop signal when docker host stop container

--- dockerfile/web/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e /workspace/wrkfree2.0/webServer/server/server.js ] ; then
-    echo "start web service ... "
-    NODE_ENV=production node /workspace/wrkfree2.0/webServer/server/server.js
-else
-    echo "server script is not exist !"
-fi
--- dockerfile/ws/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e /workspace/wrkfree2.0/webServer/server/server.js ] ; then
-    echo "start socket service ... "
-    NODE_ENV=production node /workspace/wrkfree2.0/socketServer/server.js
-else
-    echo "server script is not exist !"
-fi
