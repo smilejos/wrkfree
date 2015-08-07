f59690a9e549315809945c0da17a75f4a20ab283
GeorgeChen <bamoo456@gmail.com>
2015-08-08 00:25:40 +0800

    [Impl] add SearchHelper for implementing some supported APIs

---
+++ storageService/searches/SearchHelper.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ * parser is used to parse search results from different collections
+ */
+var Parsers = {
+    users: function(info) {
+        info._source.uid = info._id;
+        return info._source;
+    },
+    channels: function(info) {
+        info._source.channelId = info._id;
+        return info._source;
+    }
+};
+
+/**
+ * expression is a formatted query json which can be accepted by elasticsearch server
+ */
+var Expression = {
+    users: function(text) {
+        return {
+            query: {
+                multi_match: {
+                    query: text,
+                    type: 'phrase_prefix',
+                    fields: ['giveName', 'familyName', 'nickName'],
+                    prefix_length: 3,
+                    max_expansions: 10
+                }
+            }
+        };
+    },
+    channels: function(text) {
+        return {
+            query: {
+                match: {
+                    name: {
+                        query: text,
+                        type: 'phrase_prefix',
+                        fuzziness: 2,
+                        prefix_length: 2,
+                        max_expansions: 10
+                    }
+                }
+            }
+        };
+    }
+};
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: to get the search result parser based on collection
+ *
+ * @param {String}      collection, the mongo collection name
+ */
+exports.getParser = function(collection) {
+    return Parsers[collection];
+};
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: to get the search query expression based on collection
+ *
+ * @param {String}      collection, the mongo collection name
+ */
+exports.getQueryExpression = function(collection, text) {
+    var handler = Expression[collection];
+    return (!handler ? null : handler(text));
+};
