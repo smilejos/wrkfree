f11e09c6e34e870f034902bc81dc30864c9efc62
GeorgeChen <bamoo456@gmail.com>
2015-08-19 18:23:51 +0800

    [Impl] dynamically calculate the canvas widht and height

--- webServer/shared/components/mainBox/MainWorkSpace.jsx
+++ webServer/shared/components/mainBox/MainWorkSpace.jsx
@@ -1,4 +1,17 @@
 var React = require('react');
+var SharedUtils = require('../../../../sharedUtils/utils');
+
+/**
+ * load configs
+ */
+var Configs = require('../../../../configs/config');
+var BOARD_WIDTH = Configs.get().params.draw.boardWidth;
+var BOARD_HEIGHT = Configs.get().params.draw.boardHeight;
+
+if (!SharedUtils.isNumber(BOARD_WIDTH) || 
+    !SharedUtils.isNumber(BOARD_HEIGHT)) {
+    throw new Error('error while on getting draw related params');
+}
 
 /**
  * child components
@@ -6,15 +19,54 @@ var React = require('react');
 var DrawingBoard = require('./drawingBoard.jsx');
 var DrawingPalette = require('./drawingPalette.jsx');
 
+var ResizeTimeout = null;
+
 /**
  * the DrawingArea.jsx is the main container of drawing feature
  */
 module.exports = React.createClass({
+    getInitialState: function() {
+        // give the default value of canvas width and height
+        return {
+            canvasWidth: BOARD_WIDTH,
+            canvasHeight: BOARD_HEIGHT
+        };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+        self._resizeCanvas();
+        window.addEventListener('resize', function(e) {
+            if (ResizeTimeout) {
+                clearTimeout(ResizeTimeout);
+            }
+            ResizeTimeout = setTimeout(function() {
+                self._resizeCanvas();
+            }, 300);
+        });
+    },
+
+    /**
+     * @Author: George_Chen
+     * @Description: resize the current canvas
+     */
+    _resizeCanvas: function() {
+        var ratio = (BOARD_WIDTH / BOARD_HEIGHT);
+        var width = (window.innerWidth - 200) * 0.8;
+        var height = width / ratio;
+        this.setState({
+            canvasWidth: width,
+            canvasHeight: height
+        });
+    },
+
     render: function(){
         return (
             <div className="mainBox" >
                 <DrawingPalette isActive={this.props.drawInfo.drawOptions.palette}/>
                 <DrawingBoard 
+                    width={this.state.canvasWidth}
+                    height={this.state.canvasHeight}
                     channelId={this.props.channel.channelId} 
                     boardId={this.props.drawInfo.currentBoardId}
                     drawInfo={this.props.drawInfo}/>
