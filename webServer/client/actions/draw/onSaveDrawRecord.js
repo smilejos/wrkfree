d69a7035d002698d6940e92e3b8ba5c505ef959a
GeorgeChen <bamoo456@gmail.com>
2015-08-23 17:29:17 +0800

    [Impl] showing drawing tips when local and remote not stay on the same        drawingBoard.

--- webServer/client/actions/draw/onSaveDrawRecord.js
+++ webServer/client/actions/draw/onSaveDrawRecord.js
@@ -1,7 +1,13 @@
 'use strict';
 var Promise = require('bluebird');
 var SharedUtils = require('../../../../sharedUtils/utils');
+var ActionUtils = require('../actionUtils');
 var DrawTempStore = require('../../../shared/stores/DrawTempStore');
+var WorkSpaceStore = require('../../../shared/stores/WorkSpaceStore');
+var ChannelService = require('../../services/channelService');
+var NavToBoard = require('./navToBoard');
+// to indicate current tips is showing or not
+var TipsShowing = null;
 
 var Configs = require('../../../../configs/config');
 // define the maximum number of draws can be lost during client drawing
@@ -36,10 +42,14 @@ module.exports = function(actionContext, data, callback) {
         drawOptions: SharedUtils.argsCheckAsync(data.drawOptions, 'drawOptions')
     }).then(function(validData) {
         var drawTempStore = actionContext.getStore(DrawTempStore);
+        var wkStore = actionContext.getStore(WorkSpaceStore);
         var tempRecord = drawTempStore.getDraws(validData.channelId, validData.boardId, validData.clientId);
         var missingDraws = Math.abs(tempRecord.length - validData.chunksNum);
         if (missingDraws > MISSING_DRAWS_LIMIT) {
             throw new Error('record is broken');
+        }
+        if (!wkStore.isCurrentUsedBoard(data.channelId, data.boardId)) {
+            _showNavigationTips(actionContext, data.channelId, data.boardId);
         }
         return actionContext.dispatch('ON_RECORD_SAVE', {
             channelId: data.channelId,
@@ -51,6 +61,7 @@ module.exports = function(actionContext, data, callback) {
         });
     }).catch(function(err) {
         SharedUtils.printError('onSaveDrawRecord.js', 'core', err);
+        ActionUtils.showWarningEvent('WARN', 'remote drawing fail !');
         // TODO:
         // what if channel id and board id is null ?
         actionContext.dispatch('CLEAN_FAILURE_DRAW', {
@@ -61,3 +72,39 @@ module.exports = function(actionContext, data, callback) {
         return null;
     }).nodeify(callback);
 };
+
+/**
+ * @Public API
+ * @Author: George_Chen
+ * @Description: showing information tips when someone drawing on remote side
+ * 
+ * @param {Object}      actionContext, the fluxible's action context
+ * @param {String}      cid, target channel id
+ * @param {Number}      bid, target board id
+ */
+function _showNavigationTips(actionContext, cid, bid) {
+    var boardIndex = bid + 1;
+    var reqData = {
+        channelId: cid
+    };
+    if (TipsShowing) {
+        return;
+    }
+    TipsShowing = true;
+    return ChannelService
+        .getInfoAsync(reqData)
+        .delay(2000).then(function(info) {
+            var title = 'Channel: #' + info.basicInfo.name;
+            var msg = 'someone is drawing on board [' + boardIndex + ']';
+            TipsShowing = null;
+            if (!info.basicInfo.is1on1) {
+                ActionUtils.showInfoEvent(title, msg, 'quick open', function(urlNavigator) {
+                    actionContext.executeAction(NavToBoard, {
+                        urlNavigator: urlNavigator,
+                        channelId: cid,
+                        boardId: bid
+                    });
+                });
+            }
+        });
+}
