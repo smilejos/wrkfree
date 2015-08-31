9d7c1d7818be80dece99910ad8436d9dc18e174d
GeorgeChen <bamoo456@gmail.com>
2015-08-31 18:25:49 +0800

    [Impl] add client report message input bar on current app

--- webServer/shared/components/Html.jsx
+++ webServer/shared/components/Html.jsx
@@ -29,7 +29,7 @@ module.exports = React.createClass({
                 <title>{this.props.title}</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/pure/0.5.0/pure-min.css" />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.3.0/css/font-awesome.min.css"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.4.0/css/font-awesome.min.css"/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" />
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
