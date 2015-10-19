93f7ea0daac935c70e945466c3e8e3c2c24ea642
GeorgeChen <bamoo456@gmail.com>
2015-10-19 15:10:03 +0800

    [Impl] implement manually es search indexing on channels        NOTE: after switch to postgresSQL, we must index item manually

--- storageService/searches/SearchHelper.js
+++ storageService/searches/SearchHelper.js
@@ -69,3 +69,21 @@ exports.getQueryExpression = function(collection, text) {
     var handler = Expression[collection];
     return (!handler ? null : handler(text));
 };
+
+/**
+ * Public API
+ * @Author: George_Chen
+ * @Description: used for indexing db item into es search
+ *
+ * @param {String}      table, the db table name
+ * @param {String}      indexId, the item index id
+ * @param {Object}      source, the item source object
+ */
+exports.getIndexExpression = function(table, indexId, source) {
+    return {
+        index: 'wrkfree',
+        type: table,
+        id: indexId,
+        body: source
+    };
+};
