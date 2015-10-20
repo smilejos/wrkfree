3207930de7da5647ff46fa60233936135640a0da
GeorgeChen <bamoo456@gmail.com>
2015-10-20 17:25:14 +0800

    [Impl] integrate channel, member, drawboard and friend operations

--- storageService/pgDaos/PgFriend.js
+++ storageService/pgDaos/PgFriend.js
@@ -2,6 +2,7 @@
 var Promise = require('bluebird');
 var SharedUtils = require('../../sharedUtils/utils');
 var LogUtils = require('../../sharedUtils/logUtils');
+var CryptoUtils = require('../../sharedUtils/cryptoUtils');
 var LogCategory = 'STORAGE';
 var Agent = require('../pgAgent');
 
@@ -39,24 +40,41 @@ exports.getFriendsAsync = function(owner) {
  * @param {String}      user2, the user2's uid
  */
 exports.addFriendshipAsync = function(user1, user2) {
-    return Promise.all([
+    return Promise.join(
         SharedUtils.argsCheckAsync(user1, 'md5'),
-        SharedUtils.argsCheckAsync(user2, 'md5')
-    ]).then(function(queryParams) {
-        return Agent.execTransactionAsync([{
-            text: 'INSERT INTO friends(owner, uid) VALUES($1, $2)',
-            values: queryParams
-        }, {
-            text: 'INSERT INTO friends(owner, uid) VALUES($2, $1)',
-            values: queryParams
-        }]);
-    }).catch(function(err) {
-        LogUtils.error(LogCategory, {
-            args: SharedUtils.getArgs(arguments),
-            error: err.toString()
-        }, 'error in PgFriend.addFriendshipAsync()');
-        throw err;
-    });
+        SharedUtils.argsCheckAsync(user2, 'md5'),
+        function(uid1, uid2) {
+            var hosts = SharedUtils.get1on1ChannelHost(uid1, uid2);
+            var cid = CryptoUtils.getMd5Hex(hosts);
+            return Agent.execTransactionAsync([{
+                text: 'INSERT INTO friends(owner, uid) VALUES($1, $2)',
+                values: [uid1, uid2]
+            }, {
+                text: 'INSERT INTO friends(owner, uid) VALUES($1, $2)',
+                values: [uid2, uid1]
+            }, {
+                text: 'INSERT INTO channels(id, host, name, "is1on1") ' +
+                    'VALUES($1, $2, $3, $4) RETURNING * ',
+                values: [cid, hosts, '', true]
+            }, {
+                text: 'INSERT INTO drawBoards("channelId") VALUES($1)',
+                values: [cid]
+            }, {
+                text: 'INSERT INTO members(member, "channelId", "isStarred", "is1on1", "isHost", "msgSeenTime", "lastVisitTime") ' +
+                    'VALUES($1, $2, $3, $3, $3, $4, $4)',
+                values: [uid1, cid, true, new Date()]
+            }, {
+                text: 'INSERT INTO members(member, "channelId", "isStarred", "is1on1", "isHost", "msgSeenTime", "lastVisitTime") ' +
+                    'VALUES($1, $2, $3, $3, $3, $4, $4)',
+                values: [uid2, cid, true, new Date()]
+            }]);
+        }).catch(function(err) {
+            LogUtils.error(LogCategory, {
+                args: SharedUtils.getArgs(arguments),
+                error: err.toString()
+            }, 'error in PgFriend.addFriendshipAsync()');
+            throw err;
+        });
 };
 
 /**
@@ -68,24 +86,38 @@ exports.addFriendshipAsync = function(user1, user2) {
  * @param {String}      user2, the user2's uid
  */
 exports.deleteFriendshipAsync = function(user1, user2) {
-    return Promise.all([
+    return Promise.join(
         SharedUtils.argsCheckAsync(user1, 'md5'),
-        SharedUtils.argsCheckAsync(user2, 'md5')
-    ]).then(function(queryParams) {
-        return Agent.execTransactionAsync([{
-            text: 'DELETE FROM friends WHERE owner=$1 AND uid=$2',
-            values: queryParams
-        }, {
-            text: 'DELETE FROM friends WHERE owner=$2 AND uid=$1',
-            values: queryParams
-        }]);
-    }).catch(function(err) {
-        LogUtils.error(LogCategory, {
-            args: SharedUtils.getArgs(arguments),
-            error: err.toString()
-        }, 'error in PgFriend.deleteFriendshipAsync()');
-        throw err;
-    });
+        SharedUtils.argsCheckAsync(user2, 'md5'),
+        function(uid1, uid2) {
+            var hosts = SharedUtils.get1on1ChannelHost(uid1, uid2);
+            var cid = CryptoUtils.getMd5Hex(hosts);
+            return Agent.execTransactionAsync([{
+                text: 'DELETE FROM friends WHERE owner=$1 AND uid=$2',
+                values: [uid1, uid2]
+            }, {
+                text: 'DELETE FROM friends WHERE owner=$1 AND uid=$2',
+                values: [uid2, uid1]
+            }, {
+                text: 'DELETE FROM channels WHERE id=$1',
+                values: [cid]
+            }, {
+                text: 'DELETE FROM drawBoards WHERE "channelId"=$1',
+                values: [cid]
+            }, {
+                text: 'DELETE FROM drawRecords WHERE "channelId"=$1',
+                values: [cid]
+            }, {
+                text: 'DELETE FROM members WHERE "channelId"=$1',
+                values: [cid]
+            }]);
+        }).catch(function(err) {
+            LogUtils.error(LogCategory, {
+                args: SharedUtils.getArgs(arguments),
+                error: err.toString()
+            }, 'error in PgFriend.addFriendshipAsync()');
+            throw err;
+        });
 };
 
 /**
